6b814297c6fa2f2d933a75268d8c7b84116511d0
ChristophGordalla <76954754+ChristophGordalla@users.noreply.github.com>
2022-06-19 18:29:25 +0200

    Create config_mail.py

---
+++ config_mail.py
@@ -0,0 +1,5 @@
+# sensitive information that have been disguised for the repository
+# change all *'s with meaningful information
+MAIL_SENDER = '**********@***'
+MAIL_PASSWORD = ''**********'
+MAIL_RECIPENTS = ['**********@***', '**********@***']
